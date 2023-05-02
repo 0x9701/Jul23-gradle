180171a48d776a090da6ccc9f9236cf669ed2d8e
0x9701 <105629185+0x9701@users.noreply.github.com>
2023-05-02 20:00:48 +0800

    push

--- Jul23-cache/src/main/java/eth/ox9701/cache/CacheApplication.java
+++ Jul23-cache/src/main/java/eth/ox9701/cache/CacheApplication.java
@@ -2,7 +2,9 @@ package eth.ox9701.cache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CacheApplication {
     public static void main(String[] args) {
--- Jul23-exchange/src/main/java/eth/ox9701/exchange/ExchangeApplication.java
+++ Jul23-exchange/src/main/java/eth/ox9701/exchange/ExchangeApplication.java
@@ -2,7 +2,9 @@ package eth.ox9701.exchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ExchangeApplication {
     public static void main(String[] args) {
